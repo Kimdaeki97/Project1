f6a38810792b7734f91b9cd88b90533f8700ded3
Kimdaeki97 <daeki56@gmail.com>
2021-02-10 15:04:38 +0900

    pro1

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by 김대기 on 2021/02/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
